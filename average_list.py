247778860f2ea101ea3f1ab958eb573379500bbf
Jordan <4berry1@gmail.com>
2021-05-03 18:35:33 -0700

    Create average_list.py

---
+++ average_list.py
@@ -0,0 +1,10 @@
+def average(a):
+    test = []
+    if (a == test):
+        return 0
+    total = 0
+    count = 0
+    for y in a:
+        total += y
+        count += 1
+    return total / count
