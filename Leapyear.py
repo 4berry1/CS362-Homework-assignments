0d8ba0d6dc16d0e38d6db44ee227cc5f1523d53d
Jordan <4berry1@gmail.com>
2021-05-31 19:05:53 -0700

    leap years devisable by 100 are not leap years

--- Leapyear.py
+++ Leapyear.py
@@ -1,4 +1,6 @@
 def year(a):
-    if (a % 4 == 0):
+    if (a % 100 == 0):
+        return "{b} is not a leap year".format(b = a)
+    elif (a % 4 == 0):
         return "{b} is a leap year".format(b = a)
     return "{b} is not a leap year".format(b = a)
