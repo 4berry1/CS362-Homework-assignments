6ebbc20fb8d7c524a323571ce6d5d58b54a0c304
Jordan <4berry1@gmail.com>
2021-05-31 19:07:06 -0700

    added test so leap years devisable by 400 are leap years

--- test_Leapyear.py
+++ test_Leapyear.py
@@ -6,6 +6,8 @@ class testCaseAdd(unittest.TestCase):
         self.assertEqual(Leapyear.year(4), "4 is a leap year")
     def test_2(self):
         self.assertEqual(Leapyear.year(100), "100 is not a leap year")
+    def test_3(self):
+        self.assertEqual(Leapyear.year(400), "400 is a leap year")
 
 if __name__ == '__main__':
     unittest.main()
